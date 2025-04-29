60258814ac24a3c085fdfccc2b9625a75feec146
Orest Yakymiv <akimivorest@gmail.com>
2025-04-29 12:37:11 +0300

    fixed errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+cd .infrastructure
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
+kubectl apply -f confgiMap.yml
+kubectl apply -f secret.yml
+kubectl apply -f deployment.yml
+
+
+
